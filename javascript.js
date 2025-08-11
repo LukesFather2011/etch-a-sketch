aad26825d5322f2c0d844cd9d7928e056c983550
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-08-11 07:56:33 -0500

    change from 4x4 to 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
 
 // create 16 divs inside the container for further styling
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("gridItem");
     container.appendChild(div);

fd20dd70411644da7a3cdb289123b5037c76a68c
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-08-11 07:51:24 -0500

    establish 4x4 grid

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+
+// create 16 divs inside the container for further styling
+for (let i = 0; i < 16; i++) {
+    const div = document.createElement("div");
+    div.classList.add("gridItem");
+    container.appendChild(div);
+}

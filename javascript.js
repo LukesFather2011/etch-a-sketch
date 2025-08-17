a76959214ca1f451deb69ca667f8aa6982e410db
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-08-17 16:41:12 -0500

    add funcitonality to btn, divs not evenly spaced

--- javascript.js
+++ javascript.js
@@ -1,13 +1,21 @@
 const container = document.querySelector("#container");
+const button    = document.querySelector("button");
+const gridItems = document.querySelectorAll(".gridItem")
 
-// create 16 divs inside the container for further styling
-for (let i = 0; i < 256; i++) {
+
+function createGrid (divsPerSide) {
+    for (let i = 0; i < divsPerSide * divsPerSide; i++) {
     const div = document.createElement("div");
     div.classList.add("gridItem");
     container.appendChild(div);
 }
+}
 
-const gridItems = document.querySelectorAll(".gridItem")
+// I need to add functionality to add divs per side.
+button.addEventListener("click", () => {
+    const numberOfDivs = prompt("How many divs per side: ");
+    createGrid(numberOfDivs);
+});
 
 gridItems.forEach((gridItem) => {
     gridItem.addEventListener("mouseover", () => {

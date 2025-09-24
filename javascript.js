36a36957888e46489a323f829dcde9d1795986b9
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-09-24 07:41:11 -0500

    fix scaling issue, add hover effect

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
 const container = document.querySelector("#container");
 const divContainer = document.querySelector("#divContainer");
 const button    = document.querySelector("button");
-const gridItems = document.querySelectorAll(".gridItem")
+const gridItems = document.querySelectorAll(".gridItem");
 
 
 function createGrid (divsPerSide) {
+    divContainer.innerHTML = "";  // clears the grid 
     for (let i = 0; i < divsPerSide * divsPerSide; i++) {
     const div = document.createElement("div");
     div.classList.add("gridItem");
-    div.appendChild(div);
+    div.style.width = `${100 / divsPerSide}%`;
+    div.style.height = `${100 / divsPerSide}%`;
+    divContainer.appendChild(div);
+
+    // adds hover effect to the divs
+    div.addEventListener("mouseover", () => {
+    div.style.background = "red";
+});
     }
 }
 
@@ -18,8 +26,4 @@ button.addEventListener("click", () => {
     createGrid(numberOfDivs);
 });
 
-gridItems.forEach((gridItem) => {
-    gridItem.addEventListener("mouseover", () => {
-        gridItem.setAttribute("style", "background: red;")
-    })
-})
+

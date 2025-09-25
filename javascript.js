1b03a289354388699d41c7b4ad12e3eba6df62d4
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-09-25 07:24:28 -0500

    add comments

--- javascript.js
+++ javascript.js
@@ -5,21 +5,21 @@ const gridItems = document.querySelectorAll(".gridItem");
 
 
 function createGrid (divsPerSide) {
-    divContainer.innerHTML = "";  // clears the grid 
+    divContainer.innerHTML = "";  // clears the grid if pressed without input.
 
     for (let i = 0; i < divsPerSide * divsPerSide; i++) {
-    const div = document.createElement("div");
-    div.classList.add("gridItem");
+        const div = document.createElement("div");
+        div.classList.add("gridItem");
 
-    //enures divs are sized evenly
-    div.style.width = `${100 / divsPerSide}%`;
-    div.style.height = `${100 / divsPerSide}%`;
-    divContainer.appendChild(div);
+        //enures divs are sized evenly in the grid area
+        div.style.width = `${100 / divsPerSide}%`;
+        div.style.height = `${100 / divsPerSide}%`;
+        divContainer.appendChild(div);
 
-    // adds hover effect to the divs
-    div.addEventListener("mouseover", () => {
-    div.style.background = "blue";
-    });
+        // adds hover effect to the divs
+        div.addEventListener("mouseover", () => {
+        div.style.background = "blue";  // I intend to come back and change this later to do random colors. 
+        });
     }
 }
 
